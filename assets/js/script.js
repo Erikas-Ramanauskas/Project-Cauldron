9dbe3b8aed7628091984932ce42de146c6ee8fe0
Pablo Embarach <pabloembarach@gmail.com>
2023-10-22 14:36:33 +0000

    update menu navigation on game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,9 +210,10 @@ interact('.villain').dropzone({
 
 // Keyboard event for menu open
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'p' || event.key === 'P') {
-        const menu = document.getElementById('menu');
-        menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
-    }
-});
+// document.addEventListener('keydown', function (event) {
+//     if (event.key === 'p' || event.key === 'P') {
+//         const menu = document.getElementById('menu');
+//         menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
+//     }
+// });
+
